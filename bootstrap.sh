30a6665ff1852655ea6fe68403da4103e555466e
Mirco Santosuosso <mirco.santosuosso@gmail.com>
2026-03-04 21:45:09 +0100

    Update ansible-playbook command with new paths

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,4 +4,4 @@ set -e
 sudo apt update
 sudo apt install -y ansible
 
-ansible-playbook -i inventory/local site.yml --ask-become-pass
+ansible-playbook -i ansible/inventory/local ansible/site.yml --ask-become-pass
