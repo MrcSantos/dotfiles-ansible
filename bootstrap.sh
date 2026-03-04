a5b1492017a9570aabb65e1c5a97a7865a7dbb54
Mirco Santosuosso <mirco.santosuosso@gmail.com>
2026-03-04 22:20:56 +0100

    Update Ansible playbook file reference in bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,4 +4,4 @@ set -e
 sudo apt update
 sudo apt install -y ansible
 
-ansible-playbook -i ansible/inventory/local ansible/site.yml --ask-become-pass
+ansible-playbook -i ansible/inventory/local ansible/main.yml --ask-become-pass
